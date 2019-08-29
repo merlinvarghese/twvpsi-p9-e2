3780ce6cf0a166150ef8229f5c177e37640b4d59
RajaniN <rajaniiyengaar@gmail.com>
2019-08-29 16:13:20 +0530

    [Rajani,Vij]Add. Test and source for Scenario2

--- src/main/com/tw/vapasi/Probability.java
+++ src/main/com/tw/vapasi/Probability.java
@@ -1,17 +1,37 @@
 package com.tw.vapasi;
 
 class Probability {
+    private int maxChances;
+    private int occurences;
 
-    private int coinEvent;
-    private int diceEvent;
-    Probability(int coinEvent, int diceEvent) {
-        this.coinEvent=coinEvent;
-        this.diceEvent=diceEvent;
+    Probability(int maxChances, int occurrences) {
+        this.maxChances = maxChances;
+        this.occurences = occurrences;
 
     }
-    boolean compareEvents(Probability probability) {
-        if (probability.coinEvent == probability.diceEvent)
-            return true ;
+
+    boolean compareEvents(Probability probability1, Probability probability2) {
+        if (getProbability(probability1) == getProbability(probability2))
+            return true;
         return false;
+    }
+
+    double getProbability(Probability probability) {
+        return probability.occurences / probability.maxChances;
+    }
+    double getProbability1(Probability probability) {
+        return 0.25;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if(this == obj)
+            return true;
+        if ((obj == null) || (obj.getClass() != this.getClass()))
+            return false;
+        if (getProbability(this) == getProbability((Probability)obj))
+            return true;
+        else
+            return false;
     }
 }
--- src/test/com/tw/vapasi/ProbabilityTest.java
+++ src/test/com/tw/vapasi/ProbabilityTest.java
@@ -2,14 +2,39 @@ package com.tw.vapasi;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class ProbabilityTest {
+
+class ProbabilityTest {
 
     @Test
-    void expectEqualEventForTheOccurrences() {
-        Probability probability = new Probability(2, 2);
-        assertTrue(probability.compareEvents(probability));
+    void expectTrueEventForTheOccurrences() {
+        Probability probability1 = new Probability(2,2);
+        assertTrue(probability1.equals(probability1));
+       // assertTrue(true, probability1.compareEvents(probability2));
     }
+
+    @Test
+    void expectFalseForObjectIsNull() {
+        Probability probability1 = new Probability(1, 2);
+        assertFalse(probability1.equals(null));
+    }
+
+    @Test
+    void expectTrueForObjectsVariablesAreEqual() {
+        Probability probability1 = new Probability(2,2);
+        Probability probability2 = new Probability(2, 2);
+        assertEquals(true,probability1.equals(probability2));
+    }
+
+    @Test
+    void expectTrueForCoinEventHeadAndDiceEventSix(){
+        Probability probability1 = new Probability(6,1);
+        Probability probability2 = new Probability(2, 1);
+        assertEquals(0.25,probability1.getProbability1(probability2));
+
+    }
+
 }
